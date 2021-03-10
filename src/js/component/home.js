771adce0e9b8658525d93219f7b234c45fdb1fb5
mau2662 <vargas7384@gmail.com>
2021-03-10 03:24:25 +0000

    arrays listo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "bootstrap";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState("");
+	const [taskList, setTaskList] = useState([]);
+
+	const generarLista = () => {
+		return taskList.map(detalle => {
+			return generarItem(detalle);
+		});
+	};
+
+	const generarItem = detalle => {
+		return (
+			<li className="list-group-item">
+				<p className="d-inline-block text-secondary ml-5">{detalle}</p>
+				<button className="btn btn-outline-secondary float-right">
+					<i className="fas fa-times float-right"></i>
+				</button>
+			</li>
+		);
+	};
+
+	const handleOnKeyPress = e => {
+		if (e.key === "Enter") {
+			setTaskList([...taskList, task]), setTask("");
+		}
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-muted text-center"> TODOS </h1>
+			<input
+				type="text"
+				className="form-control mb-2  text-secondary "
+				placeholder="write your task"
+				value={task}
+				onChange={e => setTask(e.target.value)}
+				onKeyPress={e => handleOnKeyPress(e)}
+			/>
+			<ul className="list-group"> {generarLista()} </ul>
+			<div className="card">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item text-secondary">4 left</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
